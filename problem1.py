783de41a63d0bb51f6151e94234912d4e23886d6
sprajjwal <shashwatprajjwal@gmail.com>
2020-04-08 11:54:39 -0700

    second test added

--- problem1.py
+++ problem1.py
@@ -1,12 +1,15 @@
 def middle_node(ll_list):
+  print(ll_list.head.data)
+  if ll_list.head == None:
+    return None
   it = ll_list.head
-	mid_ptr = ll_list.head
+  mid_ptr = ll_list.head
 	
-	while it.next != None:
-		for i in range(2):
-			if it.next != None:
-				it = it.next
-		mid_ptr = mid_ptr.next
+  while it.next != None:
+    for i in range(2):
+      if it.next != None:
+        it = it.next
+    mid_ptr = mid_ptr.next
   
   return mid_ptr
 
@@ -27,4 +30,17 @@ if __name__ == '__main__':
   ll.head = node1
 
   mid = middle_node(ll)
-  print(mid.data)
+  print(mid.data) # should be 3
+
+  print("_______________________________________")
+  
+  node2 = Node(11)
+  node2.next = Node(8)
+  node2.next.next = Node(57)
+  node2.next.next.next = Node(7)
+  # node2.next.next.next.next = Node(23)
+  ll2 = LinkedList
+  ll2.head = node2
+
+  mid2 = middle_node(ll2)
+  print(mid2.data) # should be 57
